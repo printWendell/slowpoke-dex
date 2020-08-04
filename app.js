45535fbd66248fb52d85bef3a20fdbb4ab6d95e3
wendelle <superwendell46@gmail.com>
2020-08-04 18:38:12 -0400

    created view folder with pages/index.js; set express view engine to ejs; rendered index.ejs to root path

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ let PORT = process.env.PORT || 8000;
 const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory, { maxAge: 86400000 * 60 }));
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("hello from node");
+  res.render("pages/index");
 });
 
 app.listen(PORT, () => {
