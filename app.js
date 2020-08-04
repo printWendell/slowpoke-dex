1044090619b1ce8d1b0571770b183e5cd4566ccf
wendelle <superwendell46@gmail.com>
2020-08-04 18:29:49 -0400

    created public folder with styles.css; made express serve static files

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require("path");
 let PORT = process.env.PORT || 8000;
 
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory, { maxAge: 86400000 * 60 }));
+
 app.get("/", (req, res) => {
   res.send("hello from node");
 });
