3406f6538531887499f8e7b8900da185e069d8f7
wendelle <superwendell46@gmail.com>
2020-08-04 18:50:39 -0400

    defined a basic pokeRouter in routes folder; changed app.js to use router

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 let PORT = process.env.PORT || 8000;
+const pokeRoute = require("./routes/pokeRouter");
 
 const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory, { maxAge: 86400000 * 60 }));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("pages/index");
-});
+app.use("/", pokeRoute);
 
 app.listen(PORT, () => {
   console.log("node is starting...");
