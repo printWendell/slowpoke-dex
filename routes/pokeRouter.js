a7a4be668ec897d1424309a81474499dac1b928c
wendelle <superwendell46@gmail.com>
2020-08-07 18:31:22 -0400

    passed setTypeStyle to index; used function to style looped over pokemon types; defined css classes for each type; imported type.css to styles.css

--- routes/pokeRouter.js
+++ routes/pokeRouter.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const pokeList = require("../utils/pokeList.json");
+const { setTypeStyle } = require("../utils/typeStyles");
 
 router.get("/", (req, res) => {
   res.render("pages/index", {
     pokeList,
+    setTypeStyle,
   });
 });
 
