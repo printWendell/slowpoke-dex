ae37fce1e1e62305d577c3bf32e188a5fa7d9f9f
wendelle <superwendell46@gmail.com>
2020-08-06 21:32:34 -0400

    passed/mapped  pokelist json to index ejs; displayed id, name, and image

--- routes/pokeRouter.js
+++ routes/pokeRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const pokeList = require("../utils/pokeList.json");
+
 router.get("/", (req, res) => {
-  res.render("pages/index");
+  res.render("pages/index", {
+    pokeList,
+  });
 });
 
 module.exports = router;
